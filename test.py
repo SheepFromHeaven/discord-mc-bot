68fb6f37925a5cd571e5640fbb9c9589b61503f4
SheepFromHeaven <marcwissler@gmail.com>
2017-11-27 14:34:30 +0100

    stringify duration

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ ARGS.add_argument('-d', '--duration', default=120, type=int)
 args = vars(ARGS.parse_args())
 duration = args['duration']
 
-print('You asked for a server for ' + duration + ' minutes')
+print('You asked for a server for ' + str(duration) + ' minutes')
 stdout.flush()
